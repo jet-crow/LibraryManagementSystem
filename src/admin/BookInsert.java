3117cf2e57fa7b54352a5762f7f4e59b65ccba2e
陈奥杰 <1298874688@qq.com>
2022-12-16 12:26:50 +0800

    基本框架

--- src/admin/BookInsert.java
+++ src/admin/BookInsert.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class BookInsert extends JPanel {
     public BookInsert() {
-        JLabel jLabel = new JLabel("管理员新增图书");
+        JLabel jLabel = new JLabel("图书新增");
         jLabel.setBounds(230, 50, 1000,200);
         add(jLabel);
     }
---
+++ src/admin/BorrowBooksRecord.java
@@ -0,0 +1,11 @@
+package admin;
+
+import javax.swing.*;
+
+public class BorrowBooksRecord extends JPanel {
+    public BorrowBooksRecord() {
+        JLabel jLabel = new JLabel("借书记录");
+        jLabel.setBounds(230, 50, 1000,200);
+        add(jLabel);
+    }
+}
