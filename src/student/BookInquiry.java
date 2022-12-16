d3c44b5ab4772bcb485f052faf24d0db024715a0
Drunkcat7 <Miaopasc@gmail.com>
2022-12-16 15:45:05 +0800

    图书查阅界面提回去给你

--- src/student/BookInquiry.java
+++ src/student/BookInquiry.java
@@ -33,6 +33,26 @@ public class BookInquiry extends JPanel {
         searchBtn.setFont(bigFont);
         searchJpanel.add(searchBtn);
         add(searchJpanel);
-
+//        JTextField textField = new JTextField(10);
+//        textField.addFocusListener(new JTextFieldHintListener(textField, "输入要搜索的书名"));
+//        JButton button = new JButton("搜索");
+//        add(textField);
+//        add(button);
+//        DBUtils dbUtils = new DBUtils();
+//        dbUtils.getConnection();
+//        button.addActionListener(e -> {
+//            String book = textField.getText().trim();
+//            try {
+//                ResultSet resultSet = dbUtils.getStatement().executeQuery("SELECT * FROM book WHERE bookname LIKE '%" + book + "%'");
+//                while (resultSet.next()) {
+//                    System.out.println(resultSet.getString("bookname"));
+//                    System.out.println(resultSet.getString("author"));
+//                    System.out.println(resultSet.getString("allowance"));
+//                }
+//            } catch (SQLException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//
+//        });
     }
 }
